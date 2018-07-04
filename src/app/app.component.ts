b2e610af84a1ce51a9478bf3d6eb729ceda5e0fb
Ismael <ismaproco@gmail.com>
2018-07-04 12:02:29 -0600

    Initial commit for Angular 6 with material project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+  selectedValue: string = "";
+  items = [
+    { value: "0", view: "zero" },
+    { value: "1", view: "one" },
+    { value: "2", view: "Two" }
+];
 }
